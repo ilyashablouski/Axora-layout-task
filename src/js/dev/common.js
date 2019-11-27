3a20a4cfc00c5996b8b0b43a22c8d4dff2e248d5
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2019-11-27 13:42:54 +0300

    fix some styles for ie10-11

--- src/js/dev/common.js
+++ src/js/dev/common.js
@@ -1,17 +1,17 @@
 /* eslint-disable require-jsdoc */
-$(document).ready(() => {
-  $('.toogle-button').click(() => {
+$(document).ready(function() {
+  $('.toogle-button').click(function() {
     animateCSS('.goods-card:nth-child(1)', 'flipInY');
     animateCSS('.goods-card:nth-child(2)', 'flipInY');
     animateCSS('.goods-card:nth-child(3)', 'flipInY');
     $('.goods-cards').prepend($('.goods-card:last').detach());
   });
 
-  $('.toogle-button').mousedown(() => {
+  $('.toogle-button').mousedown(function() {
     $('.toogle-button').addClass('toogle-button_push');
   });
 
-  $('.toogle-button').mouseup(() => {
+  $('.toogle-button').mouseup(function() {
     $('.toogle-button').removeClass('toogle-button_push');
   });
 });
